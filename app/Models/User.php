1ba5b2f768084510e5be4c6c8dad67c1576f6ffc
digital-experts1 <info@digitalexperts.ae>
2026-02-09 21:41:19 +0200

    fix bug in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+// ✅ Filament
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +47,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // ✅ مهم جدًا: ده اللي بيمنع 403
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // اختبار مؤقت لحل المشكلة فورًا
     }
 }
